599006a23bd682007395a532959f69153d7fdaa1
Hrushikesh-5 <hrushikeshrakhe2204@gmail.com>
2024-11-05 08:33:14 +0530

    added landing page and login

---
+++ javaScripts/cricStation.js
@@ -0,0 +1,15 @@
+// Toggle between light and dark mode
+// const toggle = document.querySelector('.theme-toggle');
+// toggle.addEventListener('click', () => {
+//   document.body.classList.toggle('light-mode');
+// });
+
+function toggleMenu() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+  }
+  const startMatch = document.querySelectorAll('.cta-button');
+  // Add an event listener to only the first button (index 0)
+startMatch[1].addEventListener('click', () => {
+    window.location.href = 'matchDetails.html';
+  });
